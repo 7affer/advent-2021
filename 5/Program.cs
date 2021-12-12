4059058386860e9d00150181f91ddfcb4d8b5609
Bartłomiej Wilk <bartek.wilk@outlook.com>
2021-12-12 21:07:36 +0100

    5

---
+++ 5/Program.cs
@@ -0,0 +1,11 @@
+﻿var input = File.ReadAllText("sample");
+
+var lines = input
+  .Split("\n")
+  .Select(line => line
+    .Split(" -> ")
+    .Select(coords => coords
+      .Split(",")
+      .Select(num => Int32.Parse(num))));
+
+Console.WriteLine(input);
